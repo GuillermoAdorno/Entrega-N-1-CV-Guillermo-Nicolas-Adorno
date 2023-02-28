184eb07ca0b1d0747d95571982c247fca4d7d0fc
GuillermoAdorno <126611897+GuillermoAdorno@users.noreply.github.com>
2023-02-28 16:16:56 -0300

    Add files via upload

---
+++ scripts/motor.js
@@ -0,0 +1,10 @@
+function verMenos(){
+    document.getElementsByTagName("main")[0].style.height="1200px"
+    document.getElementById("escondido").style.display="none"
+    document.getElementById("exhibido").style.display="inline"
+}
+function verMas(){
+    document.getElementsByTagName("main")[0].style.height="2150px"
+    document.getElementById("escondido").style.display="inline"
+    document.getElementById("exhibido").style.display="none"
+}
